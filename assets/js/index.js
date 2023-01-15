b30629588756a5820acd1018d229811fd2e9dbf2
Tomislav Dukez <tomdu3@ymail.com>
2023-01-15 20:43:14 +0000

    Convert code into functions with card array size parameters

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,35 @@
-const field = [[],[]];
-const control_choice = [0,0,0]
-let choice;
-for (let i = 0; i < 2; i++) {
-    for (let j = 0; j < 3; j++) {
-        choice = Math.floor(Math.random() * 3);
-        if (control_choice[choice] === 2) {
-            while(control_choice[choice] === 2) {
-                choice = Math.floor(Math.random() * 3);
-                if (control_choice[choice] !== 2) {
-                    control_choice[choice] += 1;
-                    field[i][j] = choice;
-                    break;
-                }
-            }            
-        } else {
-            control_choice[choice] += 1;
-            field[i][j] = choice;
+function generateField(rows, columns) {
+    const field = [];
+    for (let i = 0; i < rows; i++) {
+        field.push([]);
+    }
+    const control_choice = [];
+    for (let i = 0; i < columns; i++) {
+        control_choice.push(0);
+    }
+    console.log(control_choice);
+    let choice;
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            choice = Math.floor(Math.random() * columns);
+            if (control_choice[choice] === 2) {
+                while(control_choice[choice] === 2) {
+                    choice = Math.floor(Math.random() * columns);
+                    if (control_choice[choice] !== 2) {
+                        control_choice[choice] += 1;
+                        console.log(i,j);
+                        field[i][j] = choice;
+                        break;
+                    }
+                }            
+            } else {
+                control_choice[choice] += 1;
+                field[i][j] = choice;
+            }
         }
     }
+    return field;
 }
-
-console.log(field);
-displaySolution(field);
 
 function displaySolution(arr) {
     // let cards = [[], []];
@@ -33,3 +41,9 @@ function displaySolution(arr) {
         }
     }
 };
+
+let field = generateField(2, 3)
+console.log(field);
+displaySolution(field);
+
+
