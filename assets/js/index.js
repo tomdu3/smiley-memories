3f8929be51b1714ba6b72387d0d32a2c6540d2aa
Tomislav Dukez <tomdu3@ymail.com>
2023-01-16 06:03:48 +0000

    Add changes to try generate more cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,34 @@
 function generateField(rows, columns) {
+    let fieldHtml = document.querySelector('.memory-field');
+    let html = '';
+    let cardHtml;
+    for (let i = 0; i < rows; i++) {
+        cardHtml = '';
+        for (let j = 0; j < columns; j++) {
+            cardHtml += `<div class="card" id="card-${i}${j}"></div>`;
+        }
+        html += `<div class="row" id="row-${i}">
+                ${cardHtml}
+                </div>`;
+    }
+    console.log(cardHtml);
+    console.log(html);
+    fieldHtml.innerHTML = html;
+    console.log(html)
+}
+
+// there's bug here
+function generateSolution(rows, columns) {
     const field = [];
     for (let i = 0; i < rows; i++) {
         field.push([]);
     }
+    console.log(field);
     const control_choice = [];
     for (let i = 0; i < columns; i++) {
         control_choice.push(0);
     }
-    console.log(control_choice);
+    console.log(control_choice)
     let choice;
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < columns; j++) {
@@ -31,19 +52,23 @@ function generateField(rows, columns) {
     return field;
 }
 
-function displaySolution(arr) {
+function displaySolution(arr, rows, columns) {
     // let cards = [[], []];
-    for (let i = 0; i < 2; i++) {
-        for (let j = 0; j < 3; j++) {
-            let card = document.querySelector(`#row-${i} #card-${j}`);
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            let card = document.querySelector(`#card-${i}${j}`);
             // cards[i][j] = card;
             card.innerHTML = `<p>${arr[i][j]}</p>`;
         }
     }
 };
 
-let field = generateField(2, 3)
+let numberRows = 3;
+let numberColumns = 4;
+generateField(numberRows, numberColumns);
+
+// let field = generateSolution(numberRows, numberColumns)
 console.log(field);
-displaySolution(field);
+// displaySolution(field, numberRows, numberColumns);
 
 
